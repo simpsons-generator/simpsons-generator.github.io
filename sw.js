dbc8e1478db4cdde6df0f7adcd9011e6515b9204
simpsons-generator <gadsegaderr@gmail.com>
2025-07-31 15:01:54 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,13 @@
+var GHPATH = 'https://simpsons-generator.github.io/generator.html';
+var APP_PREFIX = 'simp.gen';
+var VERSION = 'version_001';
+var URLS = [    
+  `${GHPATH}/`,
+  `${GHPATH}/index.html`,
+  `${GHPATH}/sw.js`,
+  `${GHPATH}/manifest.webmanifest`,
+  `${GHPATH}/images.png`,
+  `${GHPATH}/style.css`,
+  `${GHPATH}/script.js`,
+  `${GHPATH}/generator.html`,
+]
